93d5c814460861f77b88eb8f451433e02cdebe11
serikulynur <serikulynurlan06@gmail.com>
2024-04-05 23:27:51 +0500

    fixed some bugs in javascript

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-          const buttons = document.querySelectorAll('.button');
-          const cardElements = document.querySelectorAll('.card-main');
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const buttons = document.querySelectorAll('.button');
+            const cardElements = document.querySelectorAll('.card-main');
 
-          function updateDashboard(period) {
-              cardElements.forEach((card, index) => {
-                  const activity = data[index];
-                  const currentTimeframe = activity.timeframes[period];
-                  card.querySelector('.hours').textContent = `${currentTimeframe.current}hrs`;
-                  card.querySelector('.last').textContent = `Last week - ${currentTimeframe.previous}hrs`;
-              });
-          }
+            function updateDashboard(period) {
+                const timeFrameText = period === 'daily' ? 'Last day' :
+                                      period === 'weekly' ? 'Last week' : 'Last month';
 
-          buttons.forEach(button => {
-              button.addEventListener('click', () => {
-                  const period = button.classList.contains('button-daily') ? 'daily' :
-                                 button.classList.contains('button-weekly') ? 'weekly' : 'monthly';
-                  updateDashboard(period);
-              });
-          });
+                cardElements.forEach((card, index) => {
+                    const activity = data[index];
+                    const currentTimeframe = activity.timeframes[period];
+                    card.querySelector('.hours').textContent = `${currentTimeframe.current}hrs`;
+                    card.querySelector('.last').textContent = `${timeFrameText} - ${currentTimeframe.previous}hrs`;
+                });
+            }
 
-          // Initialize with default timeframe, e.g., weekly
-          updateDashboard('weekly');
-      })
-      .catch(error => console.error('Error loading the data:', error));
+            buttons.forEach(button => {
+                button.addEventListener('click', () => {
+                    const period = button.classList.contains('button-daily') ? 'daily' :
+                                   button.classList.contains('button-weekly') ? 'weekly' : 'monthly';
+                    updateDashboard(period);
+                });
+            });
+
+            // Initialize with default timeframe, e.g., weekly
+            updateDashboard('weekly');
+        })
+        .catch(error => console.error('Error loading the data:', error));
 });
